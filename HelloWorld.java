c7bcb1dfea4e7f7039f86f518a924f473165483c
780174514 <65063300+780174514@users.noreply.github.com>
2020-05-09 15:50:19 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
   public static void main(String[] ages){
-    System.out.print("HelloWorld,GitHub")
+    System.out.print("HelloWorld,GitHub");
+      System.out.print("000");
   }
 }
